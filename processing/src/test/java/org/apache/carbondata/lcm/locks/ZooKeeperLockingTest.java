154f91fcddb315f5907bcbf2ef7de906c102ad04
rahulforallp <rahul.kumar@knoldus.in>
2016-10-27 17:00:08 +0530

    unitTest case added - BlockInfoTest.java

--- processing/src/test/java/org/apache/carbondata/lcm/locks/ZooKeeperLockingTest.java
+++ processing/src/test/java/org/apache/carbondata/lcm/locks/ZooKeeperLockingTest.java
@@ -15,8 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *//*
-
+ */
 package org.apache.carbondata.lcm.locks;
 
 import java.io.File;
@@ -37,117 +36,108 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-*/
 /**
  * @author Administrator
- *//*
-
+ */
 public class ZooKeeperLockingTest {
 
-  int freePort;
+    int freePort;
 
-  */
-/**
-   * @throws java.lang.Exception
-   *//*
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before public void setUp() throws Exception {
+        Properties startupProperties = new Properties();
+        startupProperties.setProperty("dataDir", (new File("./target").getAbsolutePath()));
+        startupProperties.setProperty("dataLogDir", (new File("./target").getAbsolutePath()));
+        freePort = findFreePort();
+        startupProperties.setProperty("clientPort", "" + freePort);
+        QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
+        try {
+            quorumConfiguration.parseProperties(startupProperties);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-  @Before public void setUp() throws Exception {
-    Properties startupProperties = new Properties();
-    startupProperties.setProperty("dataDir", (new File("./target").getAbsolutePath()));
-    startupProperties.setProperty("dataLogDir", (new File("./target").getAbsolutePath()));
-    freePort = findFreePort();
-    startupProperties.setProperty("clientPort", "" + freePort);
-    QuorumPeerConfig quorumConfiguration = new QuorumPeerConfig();
-    try {
-      quorumConfiguration.parseProperties(startupProperties);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+        final ZooKeeperServerMain zooKeeperServer = new ZooKeeperServerMain();
+        final ServerConfig configuration = new ServerConfig();
+        configuration.readFrom(quorumConfiguration);
+        new Thread() {
+            public void run() {
+                try {
+                    zooKeeperServer.runFromConfig(configuration);
+                } catch (IOException e) {
+                    System.out.println("ZooKeeper failure");
+                }
+            }
+        }.start();
     }
 
-    final ZooKeeperServerMain zooKeeperServer = new ZooKeeperServerMain();
-    final ServerConfig configuration = new ServerConfig();
-    configuration.readFrom(quorumConfiguration);
-    new Thread() {
-      public void run() {
-        try {
-          zooKeeperServer.runFromConfig(configuration);
-        } catch (IOException e) {
-          System.out.println("ZooKeeper failure");
-        }
-      }
-    }.start();
-  }
-
-  */
-/**
-   * @throws java.lang.Exception
-   *//*
-
-  @After public void tearDown() throws Exception {
-  }
-
-  @Test public void testZooKeeperLockingByTryingToAcquire2Locks()
-      throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException,
-      SecurityException {
-
-    final CarbonProperties cp = CarbonProperties.getInstance();
-    new NonStrictExpectations(cp) {
-      {
-        cp.getProperty("/CarbonLocks");
-        result = "/carbontests";
-        cp.getProperty("spark.deploy.zookeeper.url");
-        result = "127.0.0.1:" + freePort;
-      }
-    };
-
-    ZookeeperInit zki = ZookeeperInit.getInstance("127.0.0.1:" + freePort);
-
-    CarbonTableIdentifier tableIdentifier = new CarbonTableIdentifier("dbName", "tableName", "tableId");
-    ZooKeeperLocking zkl =
-        new ZooKeeperLocking(tableIdentifier,
-            LockUsage.METADATA_LOCK);
-    Assert.assertTrue(zkl.lock());
-
-    ZooKeeperLocking zk2 = new ZooKeeperLocking(
-    		tableIdentifier, LockUsage.METADATA_LOCK);
-    Assert.assertTrue(!zk2.lock());
-
-    Assert.assertTrue(zkl.unlock());
-    Assert.assertTrue(zk2.lock());
-    Assert.assertTrue(zk2.unlock());
-  }
-
-  */
-/**
-   * For finding the free port available.
-   *
-   * @return
-   *//*
-
-  private static int findFreePort() {
-    ServerSocket socket = null;
-    try {
-      socket = new ServerSocket(0);
-      socket.setReuseAddress(true);
-      int port = socket.getLocalPort();
-      try {
-        socket.close();
-      } catch (IOException e) {
-        // Ignore IOException on close()
-      }
-      return port;
-    } catch (Exception e) {
-      // Ignore
-    } finally {
-      if (socket != null) {
-        try {
-          socket.close();
-        } catch (IOException e) {
-          throw new RuntimeException(e);
-        }
-      }
+    /**
+     * @throws java.lang.Exception
+     */
+    @After public void tearDown() throws Exception {
     }
-    return 2181;
-  }
+
+    @Test public void testZooKeeperLockingByTryingToAcquire2Locks()
+            throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException,
+            SecurityException {
+
+        final CarbonProperties cp = CarbonProperties.getInstance();
+        new NonStrictExpectations(cp) {
+            {
+                cp.getProperty("/CarbonLocks");
+                result = "/carbontests";
+                cp.getProperty("spark.deploy.zookeeper.url");
+                result = "127.0.0.1:" + freePort;
+            }
+        };
+
+        ZookeeperInit zki = ZookeeperInit.getInstance("127.0.0.1:" + freePort);
+
+        CarbonTableIdentifier tableIdentifier = new CarbonTableIdentifier("dbName", "tableName", "tableId");
+        ZooKeeperLocking zkl =
+                new ZooKeeperLocking(tableIdentifier,
+                        LockUsage.METADATA_LOCK);
+        Assert.assertTrue(zkl.lock());
+
+        ZooKeeperLocking zk2 = new ZooKeeperLocking(
+                tableIdentifier, LockUsage.METADATA_LOCK);
+        Assert.assertTrue(!zk2.lock());
+
+        Assert.assertTrue(zkl.unlock());
+        Assert.assertTrue(zk2.lock());
+        Assert.assertTrue(zk2.unlock());
+    }
+
+    /**
+     * For finding the free port available.
+     *
+     * @return
+     */
+    private static int findFreePort() {
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(0);
+            socket.setReuseAddress(true);
+            int port = socket.getLocalPort();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                // Ignore IOException on close()
+            }
+            return port;
+        } catch (Exception e) {
+            // Ignore
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return 2181;
+    }
 }
-*/
